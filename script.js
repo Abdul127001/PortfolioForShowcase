222d950c8a0abb69889e655edc6d10b9cf0f3126
Khan Abdul Karim <axel127001@gmail.com>
2024-12-01 11:42:06 +0530

    made header responsive

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello, Mr JoyBoy...");
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // alert(1)
+        if(this.scrollY > 5) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+    })
+
+    $('.navbar .menu-btn').click(function(){
+        $('.navbar .menu').toggleClass('active');
+        $('.menu-btn i').toggleClass('active');
+    })
+
+})
