2541c574e7d651bd7bbeba45023fdc64cdbabd87
Khan Abdul Karim <axel127001@gmail.com>
2024-12-02 13:32:12 +0530

    carousel and contact us page

--- script.js
+++ script.js
@@ -13,4 +13,26 @@ $(document).ready(function(){
         $('.menu-btn i').toggleClass('active');
     })
 
+
+    $('.carousel').owlCarousel({
+        margin:20,
+        loop:true,
+        autoplayTimeOut:2000,
+        autopplayHoverPause:true,
+        responsive:{
+            0 : {
+                item:1,
+                nav:false,
+            },
+            600 : {
+                item:2,
+                nav:false,
+            },
+            1000 : {
+                item:3,
+                nav:false,
+            }
+        }
+    })
+
 })
