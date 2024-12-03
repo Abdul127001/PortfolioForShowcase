cd0ba91b8249bb6e16dd30bd20fb4d3ac5acc969
Khan Abdul Karim <axel127001@gmail.com>
2024-12-03 21:25:41 +0530

    added scroll up button

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 $(document).ready(function(){
     $(window).scroll(function(){
         // alert(1)
-        if(this.scrollY > 5) {
+        if(this.scrollY > 15) {
             $('.navbar').addClass("sticky");
+           
         } else {
             $('.navbar').removeClass("sticky");
+           
         }
+
+        if(this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
+
+      
+
+
+
+    })
+
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop:0})
     })
 
     $('.navbar .menu-btn').click(function(){
