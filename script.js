814e15e1f806bc1fafa41022825eb39455b1149c
Khan Abdul Karim <axel127001@gmail.com>
2025-01-21 00:51:24 +0530

    added reload functionality

--- script.js
+++ script.js
@@ -36,8 +36,9 @@ $(document).ready(function(){
     $('.carousel').owlCarousel({
         margin:20,
         loop:true,
+        autoplay:true,
         autoplayTimeOut:2000,
-        autopplayHoverPause:true,
+        autoplayHoverPause:true,
         responsive:{
             0 : {
                 items:1,
